9e78f33bc171a493abdaaea4ae369193d1709e8c
Ajay Sahani <ajay.sahani@gmail.com>
2025-02-22 13:31:57 +0530

    work on landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const LandingPage = () => {
-    return <main>
-        <section>
-            <h1>Find Your Dream Job</h1>
-        </section>
-        <div>
-            {/* button */}
+    return (
+        <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
+            <section className="text-center">
+                <h1 className="flex flex-col items-center justify-center gradient-title">
+                    Find Your Dream Job{" "}
+                    <span>
+                        and get{" "}
+                        <img
+                            src="/logo.png"
+                            alt="Hired Logo"
+                            className="h-14 sm:h24 lg:h-32"
+                        />
+                    </span>
+                </h1>
+            </section>
+            <div>
+                {/* button */}
+                {/* carousel */}
+            </div>
+            {/* banner */}
+            <section>{/* card */}</section>
+            {/* Accordion */}
+        </main>
+    );
+};
 
-        </div>
-    </main>
-}
-
-export default LandingPage
+export default LandingPage;
