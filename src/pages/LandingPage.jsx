b97dc889852d88cd567422945d26673dfbec283a
Ajay Sahani <ajay.sahani@gmail.com>
2025-02-25 14:04:50 +0530

    work on kanding page to show carousel image

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
 import React from "react";
+import { Link } from "react-router-dom";
+import companies from '../data/companies.json'
 
 const LandingPage = () => {
     return (
         <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
             <section className="text-center">
-                <h1 className="flex flex-col items-center justify-center gradient-title">
+                <h1 className="flex flex-col items-center justify-center gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl tracking-tighter py-4">
                     Find Your Dream Job{" "}
-                    <span>
+                    <span className="flex items-center gap-2 sm:gap-6">
                         and get{" "}
                         <img
                             src="/logo.png"
@@ -15,11 +19,30 @@ const LandingPage = () => {
                         />
                     </span>
                 </h1>
+                <p className="text-grey-300 sm:mt-4 text-xs sm:text-xl">
+                    Explore thousands of job listings or find the perfect candidate
+                </p>
             </section>
-            <div>
-                {/* button */}
-                {/* carousel */}
+            <div className="flex gap-6 justify-center">
+                <Link to='/jobs'>
+                    <Button variant="blue" size="xl">Find Jobs </Button>
+                </Link>
+                <Link to='/post-job'>
+                    <Button variant="destructive" size="xl">Post a Job </Button>
+                </Link>
             </div>
+            {/* carousel */}
+            <Carousel
+                className="w-full py-10"
+            >
+                <CarouselContent>
+                    {companies.map(({ name, id, path }) => {
+                        return (<CarouselItem key={id}>
+                            <img src={path} alt={name} />
+                        </CarouselItem>)
+                    })}
+                </CarouselContent>
+            </Carousel>
             {/* banner */}
             <section>{/* card */}</section>
             {/* Accordion */}
