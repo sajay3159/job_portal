579e39fe4aba12f8d1c37f2ff3206a4a2b80ae8b
Ajay Sahani <ajay.sahani@gmail.com>
2025-02-26 16:47:33 +0530

    Work on Login functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
+import { SignedIn, SignedOut, SignIn, UserButton } from '@clerk/clerk-react'
 import { Button } from './ui/button'
+import { PenBox } from 'lucide-react'
 
 const Header = () => {
-    return (
-        <nav className='py-4  flex justify-between items-center'>
-            <Link>
-                <img src="/logo.png" className='h-20' />
-            </Link>
 
-            <header>
-                <Button variant="outline">Login</Button>
-                {/* <SignedOut>
-                    <SignInButton />
-                </SignedOut>
-                <SignedIn>
-                    <UserButton />
-                </SignedIn> */}
-            </header>
-        </nav>
+    const [showSignIn, setShowSignIn] = useState(false)
+
+    return (
+        <>
+            <nav className='py-4  flex justify-between items-center'>
+                <Link>
+                    <img src="/logo.png" className='h-20' />
+                </Link>
+
+                <div className='flex gap-8'>
+                    <SignedOut>
+                        <Button variant="outline" onClick={() => setShowSignIn(true)}>Login</Button>
+                    </SignedOut>
+                    <SignedIn>
+                        {/* Add a condition here */}
+                        <Button variant="destructive" className="rounded-full">
+                            <PenBox size={20} className='mr-2' />
+                            Post a Job
+                        </Button>
+                        <Link to="post-job"></Link>
+                        <UserButton />
+                    </SignedIn>
+                </div>
+            </nav>
+            {
+                showSignIn && (
+                    <div>
+                        <SignIn
+                            signUpForceRedirectUrl='/onboarding'
+                            fallbackRedirectUrl='/onboarding'
+                        />
+                    </div>
+                )
+            }
+        </>
     )
 }
 
